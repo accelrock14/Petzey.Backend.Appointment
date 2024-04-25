87cc3ddeb75a1479cda2fae5fb76feb4f0e21c97
akjha-17 <amankumarj17@gmail.com>
2024-04-25 15:36:40 +0530

    adding remaining entities

---
+++ Petzey.Backend.Appointment.Domain/Entities/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petzey.Backend.Appointment.Domain.Entities
+{
+    public class Appointment
+    {
+        public int AppointmentID { get; set; }
+        public int DoctorID { get; set; }
+        public int PetID { get; set; }
+        public int OwnerID { get; set; }
+        public DateTime ScheduleDate { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string ReasonForVisit { get; set; }
+        public Status Status { get; set; }
+        public Report Report { get; set; }
+        public Feedback Feedback { get; set; }
+        public List<PetIssue> Petissues { get; set; } = new List<PetIssue>();
+    }
+}
---
+++ Petzey.Backend.Appointment.Domain/Entities/PetIssue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petzey.Backend.Appointment.Domain.Entities
+{
+    public class PetIssue
+    {
+        public int PetissueID { get; set; }
+        public string IssueName { get; set; }
+    }
+}
