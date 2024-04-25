eff4b060d1076a90a3aebb8d50d85bfa32ad226f
Jeferson X <86565887+accelrock14@users.noreply.github.com>
2024-04-25 17:20:03 +0530

    created dto

---
+++ Petzey.Backend.Appointment.Domain/DTO/AppointmentCardDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Petzey.Backend.Appointment.Domain.DTO
+{
+    public class AppointmentCardDto
+    {
+        public int AppointmentID { get; set; }
+        public int DoctorID { get; set; }
+        public int PetID { get; set; }
+        public DateTime ScheduleDate { get; set; }
+    }
+}
---
+++ Petzey.Backend.Appointment.Domain/DTO/PetReportHistoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Petzey.Backend.Appointment.Domain.DTO
+{
+    public class PetReportHistoryDto
+    {
+        public int ReportID { get; set; }  //latest report
+        public int HeartRate { get; set; }
+        public float Temperature {  get; set; }
+        public float OxygenLevel { get; set; }
+        public List<Test> Tests { get; set; }
+        public List<Symptom> Symptoms { get; set;}
+        public List<Prescription> Prescriptions { get; set;}
+    }
+}
