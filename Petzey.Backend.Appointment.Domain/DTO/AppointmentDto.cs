5d68c543ad895dd96bb4afd1b236b589e16d7b78
Jeferson X <86565887+accelrock14@users.noreply.github.com>
2024-04-25 15:11:25 +0530

    temp dto

--- Petzey.Backend.Appointment.Domain/DTO/AppointmentDto.cs
+++ Petzey.Backend.Appointment.Domain/DTO/AppointmentDto.cs
@@ -9,4 +9,22 @@ namespace Petzey.Backend.Appointment.Domain.DTO
     internal class AppointmentDto
     {
     }
+
+    class AppointmentCardDto
+    {
+        int AppointmentID { get; set; }
+        int DoctorID { get; set; }
+        int PetID { get; set; }
+        DateTime ScheduleDate { get; set; }
+    }
+
+    class PetReportHistoryDto
+    {
+        int ReportID { get; set; }  //latest report
+        int HeartRate { get; set; }
+        int Temperature {  get; set; }
+        //list of tests
+        //list of symptoms
+        //list of priscriptions
+    }
 }
