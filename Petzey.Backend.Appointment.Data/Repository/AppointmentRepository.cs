46349d9d2d22ef4aef7a4f7b6d157182a3466bf1
Jeferson X <86565887+accelrock14@users.noreply.github.com>
2024-04-25 21:07:26 +0530

    added repo and interface for report

--- Petzey.Backend.Appointment.Data/Repository/AppointmentRepository.cs
+++ Petzey.Backend.Appointment.Data/Repository/AppointmentRepository.cs
@@ -1,4 +1,5 @@
-﻿using Petzey.Backend.Appointment.Domain.DTO;
+﻿using Petzey.Backend.Appointment.Domain;
+using Petzey.Backend.Appointment.Domain.DTO;
 using Petzey.Backend.Appointment.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Petzey.Backend.Appointment.Data.Repository
 {
-    public class AppointmentRepository : IAppointmentRepository
+    public class AppointmentRepository : IAppointmentRepository, IReportRepository
     {
         public AppointmentStatusCountsDto AppointmentStatusCounts()
         {
@@ -16,6 +17,38 @@ namespace Petzey.Backend.Appointment.Data.Repository
         }
 
         public List<AppointmentCardDto> FilterDateStatus(FilterParamsDto filterParams)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+        // --------------------------------------------------------------------------------
+        // Report Repository Methods
+        // --------------------------------------------------------------------------------
+
+        // Get Report details by ID
+        public Report GetReportByID(int id) 
+        { 
+            throw new NotImplementedException(); 
+        }
+
+        // Update the detials of a report
+        public void UpdateReport(Report report)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Get list of all symptoms
+        public List<Symptom> GetSymptoms()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Get list of all tests
+        public List<Test> GetTests()
         {
             throw new NotImplementedException();
         }
