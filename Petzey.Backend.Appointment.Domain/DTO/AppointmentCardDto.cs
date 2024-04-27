12311d1ff77c174faafa2e43300a37c4f3b714d4
salman <2002syeds@gmail.com>
2024-04-27 21:40:40 +0530

    added CardPet and CardVet Dtos

--- Petzey.Backend.Appointment.Domain/DTO/AppointmentCardDto.cs
+++ Petzey.Backend.Appointment.Domain/DTO/AppointmentCardDto.cs
@@ -7,6 +7,14 @@ namespace Petzey.Backend.Appointment.Domain.DTO
         public int AppointmentID { get; set; }
         public int DoctorID { get; set; }
         public int PetID { get; set; }
+        public string PetName { get; set; }
+        public int PetAge { get; set; }
+        public string PetGender { get; set; }
+        public string OwnerName { get; set; }
+        // PET PHOTO
+        public string DoctorName { get; set; }
+        public string VetSpecialization { get; set; }
+        // VET PHOTO
         public DateTime ScheduleDate { get; set; }
     }
 }
