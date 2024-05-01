c0eba7571161819eaca411484830eabbfe838743
Panduranga <pandurangaajavalageri@gmail.com>
2024-05-01 00:47:27 -0700

    simple edit

---
+++ Petzey.Backend.Appointment.Data/Repository/feedbackquestionrepo.cs
@@ -0,0 +1,34 @@
+﻿using Petzey.Backend.Appointment.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petzey.Backend.Appointment.Data.Repository
+{
+   public class feedbackquestionrepo
+    {
+        PetzeyDbContext db = new PetzeyDbContext();
+        public  List<FeedbackQuestion> getfeedbackquestion()
+        {
+            var questions = db.FeedbackQuestions.ToList();
+            return questions;
+        }
+        public FeedbackQuestion getfeedbackquestionbyid(int id)
+        {
+            FeedbackQuestion feedbackQuestion = db.FeedbackQuestions.Find(id);
+            return feedbackQuestion;
+        }
+        public void updatefeedbackquestion(int id,FeedbackQuestion feedbackQuestion)
+        {
+            db.Entry(feedbackQuestion).State = EntityState.Modified;
+
+            
+            
+                db.SaveChanges();
+            
+        }
+    }
+}
