dbaf15d9cb5007ca53866266043b6fa6ba5f1f73
HARSH221202 <harshpratapsingh4151@gmail.com>
2024-05-05 17:02:09 +0530

    Implemented Caching in appointment repository

---
+++ Petzey.Backend.Appointment.Data/Caching/AppointmentCaching.cs
@@ -0,0 +1,31 @@
+﻿using Petzey.Backend.Appointment.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petzey.Backend.Appointment.Data.Caching
+{
+    public class AppointmentCaching
+    {
+        private static readonly ObjectCache cache = MemoryCache.Default;
+        private const string AppointmentsCacheKey = "Appointments";
+
+        public static IQueryable<AppointmentDetail> GetAppointmentsFromCache()
+        {
+            return cache.Get(AppointmentsCacheKey) as IQueryable<AppointmentDetail>;
+        }
+
+        public static void CacheAppointments(IQueryable<AppointmentDetail> appointments)
+        {
+            cache.Set(AppointmentsCacheKey, appointments, DateTimeOffset.Now.AddMinutes(10));
+        }
+
+        public static void ClearAppointmentsCache()
+        {
+            cache.Remove(AppointmentsCacheKey);
+        }
+    }
+}
