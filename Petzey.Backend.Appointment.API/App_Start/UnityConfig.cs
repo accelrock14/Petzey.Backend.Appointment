6dfc278337fb86d64f9ab3ba5b53dbe374b627b1
supreeth <supreethmc1029@gmail.com>
2024-05-08 01:00:32 +0530

    created repo for auth

--- Petzey.Backend.Appointment.API/App_Start/UnityConfig.cs
+++ Petzey.Backend.Appointment.API/App_Start/UnityConfig.cs
@@ -13,12 +13,15 @@ namespace Petzey.Backend.Appointment.API
 			var container = new UnityContainer();
 
             container.RegisterType<IAppointmentRepository, AppointmentRepository>();
-            
+
+            container.RegisterType<IGraphServiceClientFactory, GraphServiceClientFactory>();
+            container.RegisterType<IGraphServiceClient, GraphServiceClientImplementation>();
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+
             // e.g. container.RegisterType<ITestService, TestService>();
-            
+
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
---
+++ Petzey.Backend.Appointment.Data/Repository/GraphServiceClientFactory.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Identity;
+using Microsoft.Graph.Beta;
+using Microsoft.Graph.Beta.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petzey.Backend.Appointment.Data.Repository
+{
+    public interface IGraphServiceClientFactory
+    {
+        GraphServiceClient CreateGraphServiceClient();
+    }
+
+    public class GraphServiceClientFactory : IGraphServiceClientFactory
+    {
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly string _tenantId;
+        private readonly string[] _scopes;
+
+        public GraphServiceClientFactory()
+        {
+            _clientId = ConfigurationManager.AppSettings["clientId"];
+            _clientSecret = ConfigurationManager.AppSettings["clientSecret"];
+            _tenantId = ConfigurationManager.AppSettings["tenantId"];
+            _scopes = new[] { "https://graph.microsoft.com/.default" };
+        }
+
+        public GraphServiceClient CreateGraphServiceClient()
+        {
+            var clientSecretCredential = new ClientSecretCredential(_tenantId, _clientId, _clientSecret);
+            var graphService = new GraphServiceClient(clientSecretCredential, _scopes);
+            return graphService;
+        }
+    }
+    
+    public interface IGraphServiceClient
+    {
+        Task<User[]> GetUsersAsync();
+    }
+    public class GraphServiceClientImplementation : IGraphServiceClient
+    {
+        private readonly IGraphServiceClientFactory _graphServiceClientFactory;
+
+        public GraphServiceClientImplementation(IGraphServiceClientFactory graphServiceClientFactory)
+        {
+            _graphServiceClientFactory = graphServiceClientFactory;
+        }
+
+        public async Task<User[]> GetUsersAsync()
+        {
+            var graphService = _graphServiceClientFactory.CreateGraphServiceClient();
+            var users = await graphService.Users.GetAsync();
+            return users.Value.ToArray();
+        }
+    }
+
+}
