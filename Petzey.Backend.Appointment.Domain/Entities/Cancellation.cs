1504b41bdc73432ee2086a9c692fe2d5277f4156
akjha-17 <amankumarj17@gmail.com>
2024-05-10 18:50:03 +0530

    cancel with reason functionality

---
+++ Petzey.Backend.Appointment.Domain/Entities/Cancellation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petzey.Backend.Appointment.Domain.Entities
+{
+    public class Cancellation
+    {
+        [Key]
+        public int CancellationId { get; set; }
+        public int AppointmentID { get; set; }
+        public string Reason_for_cancellation { get; set; }
+    }
+}
