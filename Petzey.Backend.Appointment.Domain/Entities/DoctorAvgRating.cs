82563d1b9846ee7f85a30d02380ce7d23f69b348
Panduranga <pandurangaajavalageri@gmail.com>
2024-05-13 10:03:38 -0700

    added new method to doctor rating

---
+++ Petzey.Backend.Appointment.Domain/Entities/DoctorAvgRating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petzey.Backend.Appointment.Domain.Entities
+{
+    public class DoctorAvgRating
+    {
+        public double DoctorRating {  get; set; }
+        public string DoctorId {  get; set; }
+
+        public int NumberOfRatings {  get; set; }
+    }
+}
