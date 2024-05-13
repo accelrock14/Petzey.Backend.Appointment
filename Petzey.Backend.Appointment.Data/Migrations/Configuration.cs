382704de02aec6a5948ad79416872eea76faf906
AMOGH PATIL <AMOGH PATIL@DESKTOP-PC8N705>
2024-05-13 10:10:42 +0530

    committing

--- Petzey.Backend.Appointment.Data/Migrations/Configuration.cs
+++ Petzey.Backend.Appointment.Data/Migrations/Configuration.cs
@@ -14,6 +14,7 @@
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Petzey.Backend.Appointment.Data.PetzeyDbContext context)
