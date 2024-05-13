62dcf6339a6449e06596b5b363cc413258dc7c56
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-05-13 13:55:18 +0530

    updated cardPetDto

--- Petzey.Backend.Appointment.Domain/DTO/CardPetDetailsDto.cs
+++ Petzey.Backend.Appointment.Domain/DTO/CardPetDetailsDto.cs
@@ -10,7 +10,6 @@ namespace Petzey.Backend.Appointment.Domain.DTO
     {
         public int PetID { get; set; }
         public string PetName { get; set; }
-        public int? PetAge { get; set; }
         public string PetGender { get; set; }
         public string OwnerID { get; set; }
         public string petImage { get; set; }
